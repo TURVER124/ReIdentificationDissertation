0e0823c25081506995087b2d9a5cd8c8c0ef028a
Josh <josh@UbuntuDissertation.myguest.virtualbox.org>
2023-03-20 10:33:36 +0000

    Updates

--- yolo.py
+++ yolo.py
@@ -2,20 +2,11 @@ from ultralytics import YOLO
 import numpy
 import cv2
 
-model = YOLO("yolov8n.pt", "v8")
+model = YOLO("yolov8x.pt", "v8")
 # model.train (epochs=5)
 
-# detection_output = model.predict(source = "images/test_image_close.PNG", conf=0.25, save=True)
-
-# print(detection_output)
-
-# print(detection_output[0].numpy())
-
-frame_width = 640
-frame_height = 480
-
 # cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('videos/dataset_test_vid.mp4')
+cap = cv2.VideoCapture('videos/Video3/Video3_Clip2.mp4')
 
 width  = cap.get(3)
 height  = cap.get(4)
@@ -34,16 +25,16 @@ while True:
         print("Frame not recieved. Exiting...")
         break
 
-    # frame = cv2.resize(frame, (frame_width, frame_height))
+    #Resize the video so that the detection algorithm runs slower
+    scale_fact = 0.5
+    # frame = cv2.resize(frame, (int(width*scale_fact), int(height*scale_fact)))
 
-    detect_params = model.predict(source=[frame], conf=0.40, save=False)
+    detect_params = model.predict(source=[frame], conf=0.75, save=False)
 
     params = detect_params[0].numpy()
+    player_id = 0
     if len(params) != 0:
         for count in range(len(detect_params[0])):
-            # print(count)
-
-            # print(detect_params)
 
             detections = detect_params[0].boxes
             single_detect = detections[count]
@@ -51,9 +42,17 @@ while True:
             conf = single_detect.conf.numpy()[0]
             bb = single_detect.xyxy.numpy()[0]
 
-            cv2.rectangle(frame,
-                (int(bb[0]), int(bb[1])),
-                (int(bb[2]), int(bb[3])),3)
+            if class_id == 0.0:
+                #Draw the bounding box onto each person class
+                cv2.rectangle(frame,
+                    (int(bb[0]), int(bb[1])),
+                    (int(bb[2]), int(bb[3])), (0,0,255), 2)
+                #Add the confidence rate and player id number to the bounding box
+                cv2.putText(frame, (str(player_id) + " - " + str(conf)),
+                            (int(bb[0]), int(bb[1])-10), 3, 0.6, (255,255,255))
+                
+                player_id += 1
+                
 
 
     cv2.imshow('Frame', frame)
